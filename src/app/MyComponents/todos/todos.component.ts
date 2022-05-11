ed10441a57797066dbd26ca48c7e434dfe02d2c0
Akash Meher <akashmeherofficial@gmail.com>
2022-05-11 20:15:50 +0530

    done

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos:Todo[];
+  // ------problem here---------
+  localItem: string|null;
+  constructor() { 
+    this.localItem=localStorage.getItem("todos");
+    if(this.localItem==null){
+      this.todos=[]
+    }
+    else{
+      this.todos=JSON.parse(this.localItem)
+    }
+    this.todos=[]
+   }
+
+  ngOnInit(): void {
+  }
+
+  deleteToDo(todo:Todo){
+    const index=this.todos.indexOf(todo);
+    this.todos.splice(index,1);
+    localStorage.setItem("todos",JSON.stringify(this.todos))
+  }
+  addToDo(todo:Todo){
+    const index=this.todos.indexOf(todo);
+    this.todos.push(todo);
+    localStorage.setItem("todos",JSON.stringify(this.todos))
+  }
+  toggleToDo(todo:Todo){
+    const index=this.todos.indexOf(todo);
+    this.todos[index].active=!this.todos[index].active;
+    localStorage.setItem("todos",JSON.stringify(this.todos))
+  }
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AboutComponent } from './MyComponents/about/about.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
+const routes: Routes = [
+  { path: '', component: TodosComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
